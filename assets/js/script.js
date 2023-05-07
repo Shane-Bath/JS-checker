de4ec3e8fb9768fe8a7241d906ab922ce2437c10
Shane <77701559+Shane-Bath@users.noreply.github.com>
2023-05-07 20:03:22 +0000

    Add Async function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 const API_KEY = "GDwDYLKO5khD2_snWr7L8lXwhUM";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultsModal = new bootstrap.modal(document.getElementById("resultsModal"))
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+// Make a GET request to the Api URL with the API key
+//Pass the data to a display function
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString); // await the response
+
+    const data = await response.json(); // convert to json
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
